afd7a43d591f08a726ec6e3a2453a6f44600f012
Lang-Shin <bajuracharles@gmail.com>
2026-05-09 03:03:30 +0800

    Out of 100 patients. Model's accuracy 76%

--- model.py
+++ model.py
@@ -7,16 +7,33 @@ import matplotlib.pyplot as plt
 from sklearn import tree
 
 
-
-
 df = pd.read_csv('data/edu_perp_data.csv')
 
 features = df.iloc[:, :-1]   # features of the data
 labels = df.iloc[:, -1]      # labels of the data
 
 X_train, X_test, y_train, y_test = train_test_split(features, labels,
-                                                    test_size=0.2, shuffle=True,
+                                                    test_size=0.1, shuffle=True,
                                                     random_state=42)
 
 
-# d_tree = DecisionTreeClassifier(max_depth=5, random_state=42)
+d_tree = DecisionTreeClassifier(max_depth=5, random_state=42)
+d_tree.fit(X_train, y_train)
+
+
+score = 0
+# PATIENT
+for i in range(100):
+    patient = X_test.iloc[[i]]
+    patient_label = y_test.iloc[i]
+
+    predict = d_tree.predict(patient)
+
+    print("\nPATIENT ", i+1)
+    print("Prediction : ",predict[0])
+    print("Actual Label : ",patient_label)
+
+    if predict == patient_label:
+        score += 1;
+
+    print("\nSCORE : ", score)
