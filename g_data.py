33d6e894549007c535304a5303a43141e8c26083
Lang-Shin <bajuracharles@gmail.com>
2026-05-08 23:30:57 +0800

    Feat : added vital signs data

--- g_data.py
+++ g_data.py
@@ -25,3 +25,18 @@ mental_confusion = np.random.choice(
 )
 
 
+# Generate Vital Signs
+temp = np.random.normal(37.0, 0.8, n)   # loc, scale, size
+temp = np.clip(temp, 35.0, 41.0)        # var, min, max
+
+heart_rate = np.random.normal(82, 18, n)
+heart_rate = np.clip(heart_rate, 50, 160)
+
+respiratory_rate = np.random.normal(17, 5, n)
+repiratory_rate = np.clip(respiratory_rate, 10, 40)
+
+systolic_BP = np.random.normal(118, 18, n)
+systolic_BP = np.clip(systolic_BP, 70, 180)
+
+oxygen_sat = np.random.normal(97, 2.5, n)
+oxygen_sat = np.clip(oxygen_sat, 75, 100)
